79d6d2f76616ad676f8302a18fbda9540852f6af
SeanJ0307 <104651351+SeanJ0307@users.noreply.github.com>
2023-01-12 15:31:35 -0800

    had to replace the projects
    NO VISUAL SCRIPTS SEAN

---
+++ SP2/Assets/Scripts/BehaviorComponents.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorComponents : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
---
+++ SP2/Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    int myInt = 5;
+
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
